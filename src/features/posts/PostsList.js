bbfa259832aa83566737cfed37e73064e7d1fdf2
Ivan Trajkovski <trajk.ivan@gmail.com>
2022-08-11 15:31:15 +0200

    Init

---
+++ src/features/posts/PostsList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectAllPosts } from "./postsSlice";
+
+const PostsList = () => {
+  const posts = useSelector(selectAllPosts);
+
+  const renderedPosts = posts.map((post) => {
+    return (
+      <article key={post.id}>
+        <h3>{[post.title]}</h3>
+        <p>{post.content.substring(0, 100)}</p>
+      </article>
+    );
+  });
+
+  return (
+    <section>
+      <h2>Posts</h2>
+      {renderedPosts}
+    </section>
+  );
+};
+
+export default PostsList;
